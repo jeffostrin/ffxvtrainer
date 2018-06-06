fa3a193d21b9c34f012fb706646a27d7df23941a
Jeff Ostrin <jostrin@newrelic.com>
2018-06-05 17:40:49 -0700

    Add 'next-rvr', begin adding hints for when to start rss gather, and extracting constants

--- ffclock.rb
+++ ffclock.rb
@@ -1,11 +1,28 @@
 
+# Friday 5pm -> Saturday, RVR
+# Monday 5pm -> start 2 day event
+
+
+# 2:53:52
+
+
+SECONDS_IN_MINUTE = 60
+SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60
+SECONDS_IN_DAY = SECONDS_IN_HOUR * 24
+SECONDS_IN_WEEK = SECONDS_IN_DAY * 7
+
 new_major = "New Rotation / Major Events may end"
-training = "Training <==="
+
+class MiniEvents
+  GatherRSS = "Gather RSS <==="
+  Training = "Training <==="
+end
+
+training = MiniEvents::Training
 monster_hunt = "Monster Hunt <==="
 spin = "Spin the Wheel"
 secret = "Secret Gift"
 guild_defend = "Guild Defend"
-gather_rss = "Gather RSS <==="
 guild_quests = "Guild Quests"
 guild_rss_trade = "Guild RSS Trade"
 guild_help = "Guild Help"
@@ -25,13 +42,15 @@ empire_ascend = "Empire Ascend"
 research = "Research"
 
 ghalad_settings = {
-  :training => 150,
-  :gather_25k => 70,
+  :realm => "Ghalad (ipad)",
+  :training_mins => 150,
+  :gather_rss_mins => 174,
 }
 
 kellebram_settings = {
-  :training => 150,
-  :gather_25k => 45
+  :realm => "Kellebram (iphone)",
+  :training_mins => 150,
+  :gather_rss_mins => 89,
 }
 
 hourly_extras_1 = [
@@ -66,13 +85,13 @@ hourly_event = [
   { :local=>"5pm", :event=>training },
   { :local=>"6pm", :event=>guild_help },
   { :local=>"7pm", :event=>guild_defend },
-  { :local=>"8pm", :event=>gather_rss },
+  { :local=>"8pm", :event=>MiniEvents::GatherRSS },
   { :local=>"9pm", :event=>guild_defend },
-  { :local=>"10pm", :event=>gather_rss },
+  { :local=>"10pm", :event=>MiniEvents::GatherRSS },
   { :local=>"11pm", :event=>guild_quests },
   { :local=>"12pm", :event=>training },
-  { :local=>"1am", :event=>unknown },
-  { :local=>"2am", :event=>unknown },
+  { :local=>"1am", :event=>monster_hunt },
+  { :local=>"2am", :event=>guild_rss_trade },
   { :local=>"3am", :event=>unknown },
   { :local=>"4am", :event=>training },
   { :local=>"5am", :event=>monster_hunt },
@@ -84,7 +103,7 @@ hourly_event = [
   { :local=>"11am", :event=>guild_rss_trade },
   { :local=>"12am", :event=>guild_defend },
   { :local=>"1pm", :event=>spin },
-  { :local=>"2pm", :event=>gather_rss },
+  { :local=>"2pm", :event=>MiniEvents::GatherRSS },
   { :local=>"3pm", :event=>monster_hunt },
   { :local=>"4pm", :event=>hero_quests },
 ]
@@ -188,6 +207,12 @@ def assert(condition, message)
   end
 end
 
+def humanify_days(time_in_seconds)
+  return "" if time_in_seconds < SECONDS_IN_DAY
+  days = time_in_seconds / SECONDS_IN_DAY
+  return "#{days}:"
+end
+
 def humanify(future_time)
   future_time = (future_time / 60).truncate
 
@@ -195,10 +220,13 @@ def humanify(future_time)
   return "now" if future_time <= 0
   return "in 0:#{future_time}" if future_time < 60
 
+  #puts "ft = #{future_time}"
+  days = humanify_days(future_time * 60)
+  future_time = ( (future_time * 60) % SECONDS_IN_DAY ) / 60
   hours = (future_time / 60).truncate
   minutes = (future_time % 60).truncate
   minutes = minutes.to_s.rjust(2, "0")
-  return "in #{hours}:#{minutes}"
+  return "in #{days}#{hours}:#{minutes}"
 end
 
 def build_time_model(events, hour_range)
@@ -221,12 +249,62 @@ def build_time_model(events, hour_range)
       :mini_event => events[:mini_events][hour_epoch_index][:event],
       :luna_event => events[:luna_events][hour_epoch_index],
       :four_hour_extra => events[:four_hour_extras][hour_epoch_index],
-      :hourly_notes => events[:hourly_notes][hour_epoch_index],
+      :hourly_notes => events[:hourly_notes][hour_epoch_index].clone,
     }
     result << hour_info
   end
   return result
 end
+
+def add_hints(model, realm_settings)
+  model.each_with_index do |hour_data, index|
+    if hour_data[:mini_event] == MiniEvents::GatherRSS
+      gather_rss_start_epoch = hour_data[:hour_epoch] * SECONDS_IN_HOUR
+      realm_settings.each do |realm|
+        gathering_timing = realm[:gather_rss_mins]
+        assert gathering_timing != nil, "No gathering timings"
+        earliest_start_epoch = gather_rss_start_epoch - (gathering_timing * SECONDS_IN_MINUTE)
+        earliest_start_hour_epoch = earliest_start_epoch / SECONDS_IN_HOUR
+        model.each do |hour_search|
+          if hour_search[:hour_epoch] == earliest_start_hour_epoch
+            when_time = Time.at(earliest_start_epoch)
+            hour_search[:hourly_notes] << "Start Harvest in #{realm[:realm]} at #{when_time.strftime("%I:%M")}"
+          end
+        end
+      end
+    end
+    if hour_data[:mini_event] == MiniEvents::Training
+      training_start_epoch = hour_data[:hour_epoch] * SECONDS_IN_HOUR
+      realm_settings.each do |realm|
+        training_timing = realm[:training_mins]
+        assert training_timing != nil, "No training timings"
+        earliest_start_epoch = training_start_epoch - (training_timing * SECONDS_IN_MINUTE)
+        earliest_start_hour_epoch = earliest_start_epoch / SECONDS_IN_HOUR
+        model.each do |hour_search|
+          if hour_search[:hour_epoch] == earliest_start_hour_epoch
+            when_time = Time.at(earliest_start_epoch)
+            hour_search[:hourly_notes] << "Start Training in #{realm[:realm]} at #{when_time.strftime("%I:%M")}"
+          end
+        end
+      end
+    end
+  end
+  return model
+end
+
+# 1528502400 - 1528146105
+def calculate_next_rvr(rvr)
+  utc_now = Time.now.utc
+  current_epoch = utc_now.tv_sec
+  next_rvr = rvr[:interval] - ( (current_epoch - rvr[:epoch]) % (rvr[:interval]) )
+  return next_rvr
+end
+
+rvr = {
+  :epoch => 1527897600,
+  :interval => 60 * 60 * 24 * 7,
+  :duration => 60 * 60 * 24
+}
 
 assert( (hourly_event.length == 24), "There should be 24 mini hourly_event per day")
 assert( (hourly_extras.length == 24), "There should be 24 mini hourly_extras per day")
@@ -238,24 +316,29 @@ while true
   now_local = utc_now.clone.localtime
   puts "Current time is #{now_local.strftime("%I:%M (%m-%d)")}"
 
+  next_rvr = calculate_next_rvr(rvr)
+  puts "Next RVR in #{humanify(next_rvr)}"
+
   hour_range = (-historical_hours..8)
   model = build_time_model(events, hour_range)
+  model = add_hints(model, [ ghalad_settings, kellebram_settings ])
 
-  model.each do |hour|
+  model.each do |hour_data|
     output = "  "
-    if hour[:hour_index] == 0
+    if hour_data[:hour_index] == 0
       output = "=>"
     end
 
-    output += " #{hour[:local_hour].strftime("%I")}:00#{hour[:local_hour].strftime("%P")} (#{hour[:local_hour].strftime("%m-%d")}) (#{hour[:time_from_now]}) ".ljust(29, "-")
-    output += " #{hour[:mini_event]} ".ljust(19, "-")
-    output += " #{hour[:luna_event]} ".ljust(15, "-")
-    output += " #{hour[:four_hour_extra]} ".ljust(15, "-")
-    output += " #{hour[:hourly_notes]} ".ljust(15, "-")
+    output += " #{hour_data[:local_hour].strftime("%I")}:00#{hour_data[:local_hour].strftime("%P")}"
+    output += " (#{hour_data[:local_hour].strftime("%m-%d")}) (#{hour_data[:time_from_now]}) ".ljust(29, "-")
+    output += " #{hour_data[:mini_event]} ".ljust(19, "-")
+    output += " #{hour_data[:luna_event]}".ljust(15, "-")
+    output += " #{hour_data[:four_hour_extra]} ".ljust(15, "-")
+    output += " #{hour_data[:hourly_notes]} ".ljust(15, "-")
     puts output
   end
   puts "sleeping"
-  sleep 5 until Time.now.utc > (utc_now.clone + 60 * 5)
+  sleep 5 until Time.now.utc > (utc_now.clone + 60 * 1)
 end
 
 
@@ -269,7 +352,7 @@ hourly_event_1 = [
   spin,
   secret,
   guild_defend,
-  gather_rss,
+  MiniEvents::GatherRSS,
   guild_quests,
   training,
   monster_hunt,
@@ -278,14 +361,14 @@ hourly_event_1 = [
   "#{training} #{unconfirmed}",
   guild_help,
   guild_defend,
-  gather_rss,
+  MiniEvents::GatherRSS,
   training,
   guild_quests,
   monster_hunt,
   guild_rss_trade,
   guild_defend,
   spin,
-  gather_rss,
+  MiniEvents::GatherRSS,
   monster_hunt,
   hero_quests,
 ]
