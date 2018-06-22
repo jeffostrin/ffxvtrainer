b8e39e79ff72699d3b23a4057f6ac907b2c2ecfb
Jeff Ostrin <jostrin@newrelic.com>
2018-06-21 18:10:48 -0700

    update to new event rotation

--- events_mini.rb
+++ events_mini.rb
@@ -18,6 +18,34 @@ class MiniEvents
   HourlyEvents = [
     { :local=>"5pm", :event=>MiniEvents::Training },
     { :local=>"6pm", :event=>guild_help },
+    { :local=>"7pm", :event=>unknown },
+    { :local=>"8pm", :event=>unknown },
+    { :local=>"9pm", :event=>unknown },
+    { :local=>"10pm", :event=>unknown },
+    { :local=>"11pm", :event=>unknown },
+    { :local=>"12am", :event=>secret },
+    { :local=>"1am", :event=>hero_quests },
+    { :local=>"2am", :event=>unknown },
+    { :local=>"3am", :event=>unknown },
+    { :local=>"4am", :event=>unknown },
+    { :local=>"5am", :event=>unknown },
+    { :local=>"6am", :event=>guild_quests },
+    { :local=>"7am", :event=>spin },
+    { :local=>"8am", :event=>guild_defend },
+    { :local=>"9am", :event=>unknown },
+    { :local=>"10am", :event=>secret },
+    { :local=>"11am", :event=>unknown },
+    { :local=>"12pm", :event=>MiniEvents::GatherRSS },
+    { :local=>"1pm", :event=>spin },
+    { :local=>"2pm", :event=>MiniEvents::GatherRSS },
+    { :local=>"3pm", :event=>monster_hunt },
+    { :local=>"4pm", :event=>guild_quests },
+  ]
+
+
+  HourlyEvents_OLD_2 = [
+    { :local=>"5pm", :event=>MiniEvents::Training },
+    { :local=>"6pm", :event=>guild_help },
     { :local=>"7pm", :event=>guild_defend },
     { :local=>"8pm", :event=>MiniEvents::GatherRSS },
     { :local=>"9pm", :event=>guild_defend },
@@ -26,7 +54,7 @@ class MiniEvents
     { :local=>"12pm", :event=>MiniEvents::Training },
     { :local=>"1am", :event=>monster_hunt },
     { :local=>"2am", :event=>guild_rss_trade },
-    { :local=>"3am", :event=>unknown },
+    { :local=>"3am", :event=>guild_quests + unconfirmed },
     { :local=>"4am", :event=>MiniEvents::Training },
     { :local=>"5am", :event=>monster_hunt },
     { :local=>"6am", :event=>spin },
@@ -41,7 +69,6 @@ class MiniEvents
     { :local=>"3pm", :event=>monster_hunt },
     { :local=>"4pm", :event=>hero_quests },
   ]
-
 end
 
 
