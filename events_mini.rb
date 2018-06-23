204eaf4677ed8f9cda86b5a94b93bd0ff3cd8e56
Jeff Ostrin <jostrin@newrelic.com>
2018-06-23 12:44:45 -0700

    trying to figure out new event rotation

--- events_mini.rb
+++ events_mini.rb
@@ -15,33 +15,63 @@ class MiniEvents
   unconfirmed = "{Unconfirmed (Crowd Source)}"
   unknown = "Unknown"
 
-  HourlyEvents = [
-    { :local=>"5pm", :event=>MiniEvents::Training },
-    { :local=>"6pm", :event=>guild_help },
+  HourlyEvents_STARTED_JUNE_22_LOCAL_TIME = [
+    { :local=>"5pm", :event=>unknown },
+    { :local=>"6pm", :event=>unknown },
     { :local=>"7pm", :event=>unknown },
     { :local=>"8pm", :event=>unknown },
     { :local=>"9pm", :event=>unknown },
     { :local=>"10pm", :event=>unknown },
     { :local=>"11pm", :event=>unknown },
+    { :local=>"12am", :event=>unknown },
+    { :local=>"1am", :event=>unknown },
+    { :local=>"2am", :event=>unknown },
+    { :local=>"3am", :event=>unknown },
+    { :local=>"4am", :event=>unknown },
+    { :local=>"5am", :event=>unknown },
+    { :local=>"6am", :event=>unknown },
+    { :local=>"7am", :event=>unknown },
+    { :local=>"8am", :event=>unknown },
+    { :local=>"9am", :event=>unknown },
+    { :local=>"10am", :event=>guild_defend },
+    { :local=>"11am", :event=>spin },
+    { :local=>"12pm", :event=>secret },
+    { :local=>"1pm", :event=>unknown },
+    { :local=>"2pm", :event=>unknown },
+    { :local=>"3pm", :event=>unknown },
+    { :local=>"4pm", :event=>unknown },
+  ]
+
+
+  HourlyEvents = HourlyEvents_STARTED_JUNE_22_LOCAL_TIME
+
+
+  HourlyEvents_BUSTED = [
+    { :local=>"5pm", :event=>MiniEvents::Training },
+    { :local=>"6pm", :event=>monster_hunt },
+    { :local=>"7pm", :event=>unknown },
+    { :local=>"8pm", :event=>MiniEvents::GatherRSS },
+    { :local=>"9pm", :event=>guild_defend },
+    { :local=>"10pm", :event=>MiniEvents::GatherRSS },
+    { :local=>"11pm", :event=>guild_quests },
     { :local=>"12am", :event=>secret },
     { :local=>"1am", :event=>hero_quests },
-    { :local=>"2am", :event=>unknown },
+    { :local=>"2am", :event=>MiniEvents::GatherRSS },
     { :local=>"3am", :event=>unknown },
     { :local=>"4am", :event=>unknown },
     { :local=>"5am", :event=>unknown },
     { :local=>"6am", :event=>guild_quests },
     { :local=>"7am", :event=>spin },
-    { :local=>"8am", :event=>guild_defend },
-    { :local=>"9am", :event=>unknown },
+    { :local=>"8am", :event=>MiniEvents::Training },
+    { :local=>"9am", :event=>guild_quests },
     { :local=>"10am", :event=>secret },
-    { :local=>"11am", :event=>unknown },
-    { :local=>"12pm", :event=>MiniEvents::GatherRSS },
+    { :local=>"11am", :event=>guild_rss_trade },
+    { :local=>"12pm", :event=>guild_defend },
     { :local=>"1pm", :event=>spin },
     { :local=>"2pm", :event=>MiniEvents::GatherRSS },
     { :local=>"3pm", :event=>monster_hunt },
-    { :local=>"4pm", :event=>guild_quests },
+    { :local=>"4pm", :event=>hero_quests },
   ]
-
 
   HourlyEvents_OLD_2 = [
     { :local=>"5pm", :event=>MiniEvents::Training },
