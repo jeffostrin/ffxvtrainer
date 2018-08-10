052fbbc57f8151a1b5fe73900a699c16653c2965
Jeff Ostrin <jostrin@newrelic.com>
2018-08-09 18:04:26 -0700

    trying to find pattern for new mini events

--- events_mini.rb
+++ events_mini.rb
@@ -1,9 +1,12 @@
 
 class MiniEvents
-  GatherRSS = "Gather RSS <==="
-  Training = "Training <==="
+  unknown = "Unknown"
 
-  monster_hunt = "Monster Hunt <==="
+  GatherRSS = "Gather RSS <"
+  Training = "Training <"
+  Unknown = unknown
+
+  monster_hunt = "Monster Hunt <"
   spin = "Spin the Wheel"
   secret = "Secret Gift"
   guild_defend = "Guild Defend"
@@ -14,10 +17,34 @@ class MiniEvents
   vip_quests = "VIP Quests"
 
   unconfirmed = "{Unconfirmed (Crowd Source)}"
-  unknown = "Unknown"
 
 
   HourlyEvents_TEMPLATE = [
+    { :utc=>23, :local=>"4pm", :event=>unknown },
+    { :utc=>22, :local=>"3pm", :event=>unknown },
+    { :utc=>21, :local=>"2pm", :event=>unknown },
+    { :utc=>20, :local=>"1pm", :event=>unknown },
+    { :utc=>19, :local=>"12pm", :event=>unknown },
+    { :utc=>18, :local=>"11am", :event=>unknown },
+    { :utc=>17, :local=>"10am", :event=>unknown },
+    { :utc=>16, :local=>"9am", :event=>unknown },
+    { :utc=>15, :local=>"8am", :event=>unknown },
+    { :utc=>14, :local=>"7am", :event=>unknown },
+    { :utc=>13, :local=>"6am", :event=>unknown },
+    { :utc=>12, :local=>"5am", :event=>unknown },
+    { :utc=>11, :local=>"4am", :event=>unknown },
+    { :utc=>10, :local=>"3am", :event=>unknown },
+    { :utc=>9, :local=>"2am", :event=>unknown },
+    { :utc=>8, :local=>"1am", :event=>unknown },
+    { :utc=>7, :local=>"12am", :event=>unknown },
+    { :utc=>6, :local=>"11pm", :event=>unknown },
+    { :utc=>5, :local=>"10pm", :event=>unknown },
+    { :utc=>4, :local=>"9pm", :event=>unknown },
+    { :utc=>3, :local=>"8pm", :event=>unknown },
+    { :utc=>2, :local=>"7pm", :event=>unknown },
+    { :utc=>1, :local=>"6pm", :event=>unknown },
+    { :utc=>0, :local=>"5pm", :event=>unknown },
+
     { :utc=>7, :local=>"12am", :event=>unknown },
     { :utc=>8, :local=>"1am", :event=>unknown },
     { :utc=>9, :local=>"2am", :event=>unknown },
@@ -42,6 +69,294 @@ class MiniEvents
     { :utc=>3, :local=>"8pm", :event=>unknown },
     { :utc=>4, :local=>"9pm", :event=>unknown },
     { :utc=>5, :local=>"10pm", :event=>unknown },
+    { :utc=>6, :local=>"11pm", :event=>unknown },
+  ].sort { |x,y| x[:utc] <=> y[:utc] }
+
+  HourlyEvents_DATA_ENTRY = [
+    { :utc=>0, :local=>"5pm",   :events=>[ vip_quests,   "",             vip_quests,   vip_quests,   ] },
+    { :utc=>1, :local=>"6pm",   :events=>[ "",           monster_hunt,   monster_hunt, monster_hunt, ] },
+    { :utc=>2, :local=>"7pm",   :events=>[ "",           guild_rss_help, "",           ] },
+    { :utc=>3, :local=>"8pm",   :events=>[ guild_quests, "",             guild_quests, ] },
+    { :utc=>4, :local=>"9pm",   :events=>[ guild_defend, guild_defend,   guild_quests, ] },
+    { :utc=>5, :local=>"10pm",  :events=>[ secret,       secret,         guild_defend, ] },
+    { :utc=>6, :local=>"11pm",  :events=>[ hero_quests,  hero_quests,    hero_quests,  ] },
+    { :utc=>7, :local=>"12am",  :events=>[ "",           "",             "",           ] },
+    { :utc=>8, :local=>"1am",   :events=>[ "",           guild_help,     "",           ] },
+    { :utc=>9, :local=>"2am",   :events=>[ "",           "",             "",           ] },
+    { :utc=>10, :local=>"3am",  :events=>[ vip_quests,   "",             "",           ] },
+    { :utc=>11, :local=>"4am",  :events=>[ "",           "",             "",           ] },
+    { :utc=>12, :local=>"5am",  :events=>[ "",           "",             "",           ] },
+    { :utc=>13, :local=>"6am",  :events=>[ guild_quests, guild_quests,   guild_quests, ] },
+    { :utc=>14, :local=>"7am",  :events=>[ guild_defend, guild_defend,   guild_defend, ] },
+    { :utc=>15, :local=>"8am",  :events=>[ secret,       secret,         secret,       ] },
+    { :utc=>16, :local=>"9am",  :events=>[ hero_quests,  hero_quests,    "",           ] },
+    { :utc=>17, :local=>"10am", :events=>[ "",           GatherRSS,      "",           ] },
+    { :utc=>18, :local=>"11am", :events=>[ "",           guild_help,     "",           ] },
+    { :utc=>19, :local=>"12pm", :events=>[ "",           spin,           "",           ] },
+    { :utc=>20, :local=>"1pm",  :events=>[ "",           "",             "",           ] },
+    { :utc=>21, :local=>"2pm",  :events=>[ "",           monster_hunt,   monster_hunt, ] },
+    { :utc=>22, :local=>"3pm",  :events=>[ "",           guild_rss_help, "",           ] },
+    { :utc=>23, :local=>"4pm",  :events=>[ "",           guild_quests,   guild_quests, ] },
+
+  ]
+
+  HourlyEvents_LONG_FORM = [
+    [ { :utc=>23, :local=>"4pm", :event=>unknown },
+      { :utc=>22, :local=>"3pm", :event=>unknown },
+      { :utc=>21, :local=>"2pm", :event=>unknown },
+      { :utc=>20, :local=>"1pm", :event=>unknown },
+      { :utc=>19, :local=>"12pm", :event=>unknown },
+      { :utc=>18, :local=>"11am", :event=>unknown },
+      { :utc=>17, :local=>"10am", :event=>unknown },
+      { :utc=>16, :local=>"9am", :event=>unknown },
+      { :utc=>15, :local=>"8am", :event=>unknown },
+      { :utc=>14, :local=>"7am", :event=>unknown },
+      { :utc=>13, :local=>"6am", :event=>unknown },
+      { :utc=>12, :local=>"5am", :event=>unknown },
+      { :utc=>11, :local=>"4am", :event=>unknown },
+      { :utc=>10, :local=>"3am", :event=>unknown },
+      { :utc=>9, :local=>"2am", :event=>unknown },
+      { :utc=>8, :local=>"1am", :event=>unknown },
+      { :utc=>7, :local=>"12am", :event=>unknown },
+      { :utc=>6, :local=>"11pm", :event=>unknown },
+      { :utc=>5, :local=>"10pm", :event=>guild_defend },
+      { :utc=>4, :local=>"9pm", :event=>guild_quests },
+      { :utc=>3, :local=>"8pm", :event=>guild_quests },
+      { :utc=>2, :local=>"7pm", :event=>unknown },
+      { :utc=>1, :local=>"6pm", :event=>monster_hunt },
+      { :utc=>0, :local=>"5pm", :event=>vip_quests },
+    ],
+
+    [ { :utc=>23, :local=>"4pm", :event=>guild_quests },
+      { :utc=>22, :local=>"3pm", :event=>guild_rss_help },
+      { :utc=>21, :local=>"2pm", :event=>monster_hunt },
+      { :utc=>20, :local=>"1pm", :event=>unknown },
+      { :utc=>19, :local=>"12pm", :event=>spin },
+      { :utc=>18, :local=>"11am", :event=>guild_help },
+      { :utc=>17, :local=>"10am", :event=>GatherRSS },
+      { :utc=>16, :local=>"9am", :event=>hero_quests },
+      { :utc=>15, :local=>"8am", :event=>secret },
+      { :utc=>14, :local=>"7am", :event=>guild_defend },
+      { :utc=>13, :local=>"6am", :event=>guild_quests },
+      { :utc=>12, :local=>"5am", :event=>unknown },
+      { :utc=>11, :local=>"4am", :event=>unknown },
+      { :utc=>10, :local=>"3am", :event=>unknown },
+      { :utc=>9, :local=>"2am", :event=>unknown },
+      { :utc=>8, :local=>"1am", :event=>guild_help },
+      { :utc=>7, :local=>"12am", :event=>unknown },
+
+      # Aug 7
+      { :utc=>6, :local=>"11pm", :event=>hero_quests },
+      { :utc=>5, :local=>"10pm", :event=>secret },
+      { :utc=>4, :local=>"9pm", :event=>guild_defend },
+      { :utc=>3, :local=>"8pm", :event=>unknown },
+      { :utc=>2, :local=>"7pm", :event=>guild_rss_help },
+      { :utc=>1, :local=>"6pm", :event=>monster_hunt },
+      { :utc=>0, :local=>"5pm", :event=>unknown },
+    ],
+
+    [ { :utc=>23, :local=>"4pm", :event=>unknown },
+      { :utc=>22, :local=>"3pm", :event=>unknown },
+      { :utc=>21, :local=>"2pm", :event=>unknown },
+      { :utc=>20, :local=>"1pm", :event=>unknown },
+      { :utc=>19, :local=>"12pm", :event=>unknown },
+      { :utc=>18, :local=>"11am", :event=>unknown },
+      { :utc=>17, :local=>"10am", :event=>unknown },
+      { :utc=>16, :local=>"9am", :event=>hero_quests },
+      { :utc=>15, :local=>"8am", :event=>secret },
+      { :utc=>14, :local=>"7am", :event=>guild_defend },
+      { :utc=>13, :local=>"6am", :event=>guild_quests },
+      { :utc=>12, :local=>"5am", :event=>unknown },
+      { :utc=>11, :local=>"4am", :event=>unknown },
+      { :utc=>10, :local=>"3am", :event=>vip_quests },
+      { :utc=>9, :local=>"2am", :event=>unknown },
+      { :utc=>8, :local=>"1am", :event=>unknown },
+
+      # Aug 6
+      { :utc=>7, :local=>"12am", :event=>unknown },
+      { :utc=>6, :local=>"11pm", :event=>hero_quests },
+      { :utc=>5, :local=>"10pm", :event=>secret },
+      { :utc=>4, :local=>"9pm", :event=>guild_defend },
+      { :utc=>3, :local=>"8pm", :event=>guild_quests },
+      { :utc=>2, :local=>"7pm", :event=>unknown },
+      { :utc=>1, :local=>"6pm", :event=>unknown },
+      { :utc=>0, :local=>"5pm", :event=>vip_quests },
+    ],
+  ]
+
+
+  HourlyEvents_AUG_6 = [
+    { :utc=>7, :local=>"12am", :event=>unknown },
+    { :utc=>8, :local=>"1am", :event=>unknown },
+    { :utc=>9, :local=>"2am", :event=>unknown },
+    { :utc=>10, :local=>"3am", :event=>vip_quests },
+    { :utc=>11, :local=>"4am", :event=>unknown },
+    { :utc=>12, :local=>"5am", :event=>unknown },
+    { :utc=>13, :local=>"6am", :event=>guild_quests },
+    { :utc=>14, :local=>"7am", :event=>guild_defend },
+    { :utc=>15, :local=>"8am", :event=>secret },
+    { :utc=>16, :local=>"9am", :event=>hero_quests },
+    { :utc=>17, :local=>"10am", :event=>unknown },
+    { :utc=>18, :local=>"11am", :event=>unknown },
+    { :utc=>19, :local=>"12pm", :event=>unknown },
+    { :utc=>20, :local=>"1pm", :event=>unknown },
+    { :utc=>21, :local=>"2pm", :event=>unknown },
+    { :utc=>22, :local=>"3pm", :event=>unknown },
+    { :utc=>23, :local=>"4pm", :event=>unknown },
+
+    { :utc=>0, :local=>"5pm", :event=>vip_quests },
+    { :utc=>1, :local=>"6pm", :event=>unknown },
+    { :utc=>2, :local=>"7pm", :event=>unknown },
+    { :utc=>3, :local=>"8pm", :event=>guild_quests },
+    { :utc=>4, :local=>"9pm", :event=>guild_defend },
+    { :utc=>5, :local=>"10pm", :event=>secret },
+    { :utc=>6, :local=>"11pm", :event=>hero_quests },
+  ].sort { |x,y| x[:utc] <=> y[:utc] }
+
+  HourlyEvents_AUG_5 = [
+    { :utc=>7, :local=>"12am", :event=>unknown },
+    { :utc=>8, :local=>"1am", :event=>guild_help },
+    { :utc=>9, :local=>"2am", :event=>unknown },
+    { :utc=>10, :local=>"3am", :event=>unknown },
+    { :utc=>11, :local=>"4am", :event=>unknown },
+    { :utc=>12, :local=>"5am", :event=>guild_rss_help },
+    { :utc=>13, :local=>"6am", :event=>guild_quests },
+    { :utc=>14, :local=>"7am", :event=>guild_defend },
+    { :utc=>15, :local=>"8am", :event=>secret },
+    { :utc=>16, :local=>"9am", :event=>unknown },
+    { :utc=>17, :local=>"10am", :event=>GatherRSS },
+    { :utc=>18, :local=>"11am", :event=>unknown },
+    { :utc=>19, :local=>"12pm", :event=>spin },
+    { :utc=>20, :local=>"1pm", :event=>unknown },
+    { :utc=>21, :local=>"2pm", :event=>unknown },
+    { :utc=>22, :local=>"3pm", :event=>unknown },
+    { :utc=>23, :local=>"4pm", :event=>guild_quests },
+
+    { :utc=>0, :local=>"5pm", :event=>unknown },
+    { :utc=>1, :local=>"6pm", :event=>unknown },
+    { :utc=>2, :local=>"7pm", :event=>unknown },
+    { :utc=>3, :local=>"8pm", :event=>unknown },
+    { :utc=>4, :local=>"9pm", :event=>unknown },
+    { :utc=>5, :local=>"10pm", :event=>secret },
+    { :utc=>6, :local=>"11pm", :event=>secret },
+  ].sort { |x,y| x[:utc] <=> y[:utc] }
+
+
+  HourlyEvents_AUG_4 = [
+    { :utc=>7, :local=>"12am", :event=>unknown },
+    { :utc=>8, :local=>"1am", :event=>unknown },
+    { :utc=>9, :local=>"2am", :event=>unknown },
+    { :utc=>10, :local=>"3am", :event=>unknown },
+    { :utc=>11, :local=>"4am", :event=>unknown },
+    { :utc=>12, :local=>"5am", :event=>unknown },
+    { :utc=>13, :local=>"6am", :event=>unknown },
+    { :utc=>14, :local=>"7am", :event=>guild_defend },
+    { :utc=>15, :local=>"8am", :event=>secret },
+    { :utc=>16, :local=>"9am", :event=>unknown },
+    { :utc=>17, :local=>"10am", :event=>unknown },
+    { :utc=>18, :local=>"11am", :event=>guild_help },
+    { :utc=>19, :local=>"12pm", :event=>unknown },
+    { :utc=>20, :local=>"1pm", :event=>unknown },
+    { :utc=>21, :local=>"2pm", :event=>unknown },
+    { :utc=>22, :local=>"3pm", :event=>unknown },
+    { :utc=>23, :local=>"4pm", :event=>unknown },
+
+    { :utc=>0, :local=>"5pm", :event=>unknown },
+    { :utc=>1, :local=>"6pm", :event=>unknown },
+    { :utc=>2, :local=>"7pm", :event=>unknown },
+    { :utc=>3, :local=>"8pm", :event=>unknown },
+    { :utc=>4, :local=>"9pm", :event=>unknown },
+    { :utc=>5, :local=>"10pm", :event=>secret },
+    { :utc=>6, :local=>"11pm", :event=>hero_quests },
+  ].sort { |x,y| x[:utc] <=> y[:utc] }
+
+
+
+
+  HourlyEvents_AUG_3 = [
+    { :utc=>7, :local=>"12am", :event=>GatherRSS },
+    { :utc=>8, :local=>"1am", :event=>unknown },
+    { :utc=>9, :local=>"2am", :event=>unknown },
+    { :utc=>10, :local=>"3am", :event=>unknown },
+    { :utc=>11, :local=>"4am", :event=>unknown },
+    { :utc=>12, :local=>"5am", :event=>unknown },
+    { :utc=>13, :local=>"6am", :event=>unknown },
+    { :utc=>14, :local=>"7am", :event=>guild_defend },
+    { :utc=>15, :local=>"8am", :event=>secret },
+    { :utc=>16, :local=>"9am", :event=>unknown },
+    { :utc=>17, :local=>"10am", :event=>GatherRSS },
+    { :utc=>18, :local=>"11am", :event=>unknown },
+    { :utc=>19, :local=>"12pm", :event=>unknown },
+    { :utc=>20, :local=>"1pm", :event=>unknown },
+    { :utc=>21, :local=>"2pm", :event=>unknown },
+    { :utc=>22, :local=>"3pm", :event=>guild_rss_help },
+    { :utc=>23, :local=>"4pm", :event=>unknown },
+
+    { :utc=>0, :local=>"5pm", :event=>unknown },
+    { :utc=>1, :local=>"6pm", :event=>unknown },
+    { :utc=>2, :local=>"7pm", :event=>unknown },
+    { :utc=>3, :local=>"8pm", :event=>unknown },
+    { :utc=>4, :local=>"9pm", :event=>unknown },
+    { :utc=>5, :local=>"10pm", :event=>unknown },
+    { :utc=>6, :local=>"11pm", :event=>hero_quests },
+  ].sort { |x,y| x[:utc] <=> y[:utc] }
+
+
+
+  HourlyEvents_AUG_1 = [
+    { :utc=>7, :local=>"12am", :event=>unknown },
+    { :utc=>8, :local=>"1am", :event=>unknown },
+    { :utc=>9, :local=>"2am", :event=>unknown },
+    { :utc=>10, :local=>"3am", :event=>unknown },
+    { :utc=>11, :local=>"4am", :event=>unknown },
+    { :utc=>12, :local=>"5am", :event=>unknown },
+    { :utc=>13, :local=>"6am", :event=>unknown },
+    { :utc=>14, :local=>"7am", :event=>guild_quests },
+    { :utc=>15, :local=>"8am", :event=>secret },
+    { :utc=>16, :local=>"9am", :event=>hero_quests },
+    { :utc=>17, :local=>"10am", :event=>unknown },
+    { :utc=>18, :local=>"11am", :event=>unknown },
+    { :utc=>19, :local=>"12pm", :event=>unknown },
+    { :utc=>20, :local=>"1pm", :event=>unknown },
+    { :utc=>21, :local=>"2pm", :event=>unknown },
+    { :utc=>22, :local=>"3pm", :event=>unknown },
+    { :utc=>23, :local=>"4pm", :event=>guild_quests },
+
+    { :utc=>0, :local=>"5pm", :event=>unknown },
+    { :utc=>1, :local=>"6pm", :event=>monster_hunt },
+    { :utc=>2, :local=>"7pm", :event=>guild_rss_help },
+    { :utc=>3, :local=>"8pm", :event=>guild_rss_help },
+    { :utc=>4, :local=>"9pm", :event=>unknown },
+    { :utc=>5, :local=>"10pm", :event=>secret },
+    { :utc=>6, :local=>"11pm", :event=>unknown },
+  ].sort { |x,y| x[:utc] <=> y[:utc] }
+
+
+  HourlyEvents_JULY_31 = [
+    { :utc=>7, :local=>"12am", :event=>unknown },
+    { :utc=>8, :local=>"1am", :event=>unknown },
+    { :utc=>9, :local=>"2am", :event=>unknown },
+    { :utc=>10, :local=>"3am", :event=>unknown },
+    { :utc=>11, :local=>"4am", :event=>unknown },
+    { :utc=>12, :local=>"5am", :event=>unknown },
+    { :utc=>13, :local=>"6am", :event=>unknown },
+    { :utc=>14, :local=>"7am", :event=>unknown },
+    { :utc=>15, :local=>"8am", :event=>unknown },
+    { :utc=>16, :local=>"9am", :event=>unknown },
+    { :utc=>17, :local=>"10am", :event=>unknown },
+    { :utc=>18, :local=>"11am", :event=>guild_help },
+    { :utc=>19, :local=>"12pm", :event=>spin },
+    { :utc=>20, :local=>"1pm", :event=>spin },
+    { :utc=>21, :local=>"2pm", :event=>monster_hunt },
+    { :utc=>22, :local=>"3pm", :event=>guild_rss_help },
+    { :utc=>23, :local=>"4pm", :event=>unknown },
+
+    { :utc=>0, :local=>"5pm", :event=>vip_quests },
+    { :utc=>1, :local=>"6pm", :event=>unknown },
+    { :utc=>2, :local=>"7pm", :event=>unknown },
+    { :utc=>3, :local=>"8pm", :event=>guild_quests },
+    { :utc=>4, :local=>"9pm", :event=>guild_defend },
+    { :utc=>5, :local=>"10pm", :event=>secret },
     { :utc=>6, :local=>"11pm", :event=>unknown },
   ].sort { |x,y| x[:utc] <=> y[:utc] }
 
