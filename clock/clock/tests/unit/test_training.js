58947f2fdfbc6aa818005a3ad0b815fffd561991
Jeff Ostrin <jostrin@newrelic.com>
2018-09-25 17:08:06 -0700

    add training tests

--- clock/clock/tests/unit/test_training.js
+++ clock/clock/tests/unit/test_training.js
@@ -17,7 +17,7 @@ describe('Test Calculator', function () {
 
     var rateCalculator = new RateCalculator();
     var trainingRate = rateCalculator.troopCapacity(11530).trainingTime(11, 7, 15).powerPerSecond();
-    console.log(trainingRate);
+    //console.log(trainingRate);
     var troopCalculator = new TroopCalculator(11530, trainingRate);
 
     expect(troopCalculator.calculateSecondsFor("wmc", 5000)).to.equal(8681);
@@ -30,13 +30,27 @@ describe('Test Calculator', function () {
 
     var rateCalculator = new RateCalculator();
     var trainingRate = rateCalculator.troopCapacity(12100).trainingTime(11, 21, 19).powerPerSecond();
-    console.log(trainingRate);
+    //console.log(trainingRate);
     var troopCalculator = new TroopCalculator(12100, trainingRate);
 
     expect(troopCalculator.calculateSecondsFor("wmc", 5000)).to.equal(8447);
     expect(troopCalculator.calculateSecondsFor("s", 5000)).to.equal(5631);
     expect(troopCalculator.calculateSecondsFor("wmc", 15000)).to.equal(25339);
     expect(troopCalculator.calculateSecondsFor("s", 15000)).to.equal(16893);
+  });
+
+
+  it('Calculates Umpires Numbers - Sept 25 18', async () => {
+
+    var rateCalculator = new RateCalculator();
+    var trainingRate = rateCalculator.troopCapacity(12100).trainingTime(11, 16, 45).powerPerSecond();
+    //console.log(trainingRate);
+    var troopCalculator = new TroopCalculator(12100, trainingRate);
+
+    expect(troopCalculator.calculateSecondsFor("wmc", 5000)).to.equal(8390);
+    expect(troopCalculator.calculateSecondsFor("s", 5000)).to.equal(5593);
+    expect(troopCalculator.calculateSecondsFor("wmc", 15000)).to.equal(25169);
+    expect(troopCalculator.calculateSecondsFor("s", 15000)).to.equal(16779);
   });
 
 });
