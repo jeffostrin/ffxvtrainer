d47330a7806d0e9e09ad5842a2e77c26ffaba7d8
Jeff Ostrin <jostrin@newrelic.com>
2019-01-03 21:32:53 -0800

    break terraform into modules

--- terraform/ffxvtrainer_api.tf
+++ terraform/ffxvtrainer_api.tf
@@ -1,4 +1,4 @@
-resource "aws_api_gateway_rest_api" "get_schedule_api_gateway" {
+resource "aws_api_gateway_rest_api" "ffxv_trainer_api_gateway" {
   name        = "FFXV Trainer API"
   description = "This is the API for FFXV Trainer"
 
