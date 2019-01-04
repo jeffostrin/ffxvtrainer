662cc44585aaad10b0217f6228d09c6a78b1ed98
Jeff Ostrin <jostrin@newrelic.com>
2019-01-03 21:45:39 -0800

    rename terraform objects

--- terraform/get_schedule_api_integration.tf
+++ terraform/get_schedule_api_integration.tf
@@ -4,17 +4,17 @@ resource "aws_api_gateway_resource" "schedule" {
   path_part   = "schedule"
 }
 
-resource "aws_api_gateway_method" "method2" {
+resource "aws_api_gateway_method" "get_method" {
   rest_api_id   = "${aws_api_gateway_rest_api.ffxv_trainer_api_gateway.id}"
   resource_id   = "${aws_api_gateway_resource.schedule.id}"
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_response" "OK2" {
+resource "aws_api_gateway_method_response" "ok_response" {
   rest_api_id = "${aws_api_gateway_rest_api.ffxv_trainer_api_gateway.id}"
   resource_id = "${aws_api_gateway_resource.schedule.id}"
-  http_method = "${aws_api_gateway_method.method2.http_method}"
+  http_method = "${aws_api_gateway_method.get_method.http_method}"
   status_code = "200"
 
   response_models = {
@@ -22,26 +22,26 @@ resource "aws_api_gateway_method_response" "OK2" {
   }
 }
 
-resource "aws_api_gateway_integration" "integration2" {
+resource "aws_api_gateway_integration" "api_integration" {
   depends_on = [
     "aws_lambda_permission.get_schedule_lambda_function_permission"
   ]
   rest_api_id = "${aws_api_gateway_rest_api.ffxv_trainer_api_gateway.id}"
   resource_id = "${aws_api_gateway_resource.schedule.id}"
-  http_method = "${aws_api_gateway_method.method2.http_method}"
+  http_method = "${aws_api_gateway_method.get_method.http_method}"
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
   uri = "${aws_lambda_function.get_schedule_lambda_function.invoke_arn}"
 }
 
-resource "aws_api_gateway_integration_response" "integration2-response" {
+resource "aws_api_gateway_integration_response" "api_integration_response" {
   depends_on = [
-    "aws_api_gateway_integration.integration2"
+    "aws_api_gateway_integration.api_integration"
   ]
   rest_api_id = "${aws_api_gateway_rest_api.ffxv_trainer_api_gateway.id}"
   resource_id = "${aws_api_gateway_resource.schedule.id}"
-  http_method = "${aws_api_gateway_method.method2.http_method}"
-  status_code = "${aws_api_gateway_method_response.OK2.status_code}"
+  http_method = "${aws_api_gateway_method.get_method.http_method}"
+  status_code = "${aws_api_gateway_method_response.ok_response.status_code}"
 }
 
 resource "aws_lambda_permission" "get_schedule_lambda_function_permission" {
