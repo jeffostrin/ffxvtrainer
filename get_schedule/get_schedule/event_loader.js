fd5af15acf2bdb0a136c4e83ad3bd86bc95ba253
Jeff Ostrin <jostrin@newrelic.com>
2019-01-10 06:39:09 -0800

    remove some unused functions from event_loader

--- get_schedule/get_schedule/event_loader.js
+++ get_schedule/get_schedule/event_loader.js
@@ -6,32 +6,6 @@ const path = require('path');
 module.exports = function EventLoader() {
   var loader = {};
 
-  function combine_by_hour(json) {
-  	var result = [24];
-  	for (var i = 0; i <24; i++) {
-  		result[i] = [];
-  	}
-  	Object.keys(json).forEach(function(key) {
-  	  //console.log(key);
-  	  var hepoch = parseInt(key, 10);
-  	  var hour = (parseInt(key, 10) % 24);
-   	  var eventsForHour = result[hour];
-  	  json[key].forEach(function(val) {
-  	  	eventsForHour.push(val);
-   	  })
-  	});
-
-  	return result;
-  }
-
-  // https://coderwall.com/p/nilaba/simple-pure-javascript-array-unique-method-with-5-lines-of-code
-  // does not work with objects and mixed value arrays
-  // Array.prototype.unique = function() {
-  //   return this.filter(function (value, index, self) {
-  //     return self.indexOf(value) === index;
-  //   });
-  // }
-
   function read_all_hours(json) {
     var mostRecentHepoch = 0;
     Object.keys(json).forEach(function(key) {
@@ -56,56 +30,11 @@ module.exports = function EventLoader() {
   	return result;
   }
 
-  function pick_most_common_in_hour(events) {
-  	var counts = {};
-  	var mostCommon = "";
-  	var mostCommonCount = 0;
-  	events.forEach(function(event) {
-      if (counts[event] == null) {
-      	counts[event] = 0;
-      }
-
-      var count = counts[event];
-      count = count + 1;
-      counts[event] = count;
-
-      if (count > mostCommonCount) {
-        mostCommon = event;
-        mostCommonCount = count;
-	  }
-  	});
-  	return mostCommon;
-  }
-
-  function pick_most_common(byHour) {
-  	var result = [];
-  	for (var i = 0; i < 24; i++) {
-  		result[i] = pick_most_common_in_hour(byHour[i]);
-  	}
-  	return result;
-  }
-
-  loader._load = function(json) {
-  	//console.log(json);
-  	var byHour = combine_by_hour(json);
-  	//console.log(byHour);
-  	var mostCommon = pick_most_common(byHour);
-  	//console.log(mostCommon);
-  	return mostCommon;
-  }
-
   loader._loadv2 = function(json) {
   	//console.log(json);
   	var byHour = read_all_hours(json);
   	//console.log(byHour);
   	return byHour;
-  }
-
-  loader.load = function() {
-  	var filePath = path.join(__dirname, "..", "..", "mini_events.json")
-  	//console.log(filePath);
-  	var json = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-  	return loader._load(json);
   }
 
   loader._createEvaluator = function(hourlyEvents) {
