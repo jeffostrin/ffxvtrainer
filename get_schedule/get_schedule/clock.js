d66aed1d864b7c10d780ea0a7cfac754b0576777
Jeff Ostrin <jostrin@newrelic.com>
2019-01-15 10:42:47 -0800

    slight change to json format

--- get_schedule/get_schedule/clock.js
+++ get_schedule/get_schedule/clock.js
@@ -14,15 +14,15 @@ module.exports = function Clock() {
     var nowSepoch = ctime.epochSeconds();
 
     var schedule = {};
-    schedule.events = {};
+    schedule.hepochs = {};
     for (var hepoch = nowHepoch; hepoch <= nowHepoch + 24; hepoch++) {
       var jsonHour = {};
       jsonHour.hepoch = hepoch;
       jsonHour.isCurrentHepoch = (hepoch == nowHepoch);
       jsonHour.dayTime = ctime.pp().dayTime(hepoch);
       jsonHour.relativeTime = ctime.pp().asRelativeTime(hepoch * 60 * 60 - nowSepoch);
-      jsonHour.events = forecaster.forecastFor(hepoch);;
-      schedule.events[hepoch] = jsonHour;
+      jsonHour.hourly_events = forecaster.forecastFor(hepoch);;
+      schedule.hepochs[hepoch] = jsonHour;
     }
 
     return schedule;
