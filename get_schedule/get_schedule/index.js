1b3aae9e619868ee79e75f1a9c1620cb4c881066
Jeff Ostrin <jostrin@newrelic.com>
2019-01-15 10:44:59 -0800

    add cors header to response

--- get_schedule/get_schedule/index.js
+++ get_schedule/get_schedule/index.js
@@ -6,10 +6,9 @@ exports.lambda_handler_v2 = async (event, context, callback) => {
     try {
         const clock = new Clock();
         response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                schedule: clock.generate_json(),
-            })
+            statusCode: 200,
+            body: JSON.stringify({ schedule: clock.generate_json() }),
+            headers: { 'Access-Control-Allow-Origin': '*', },
         }
     }
     catch (err) {
