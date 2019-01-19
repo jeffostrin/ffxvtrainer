8a3509b02e0d42f7ecae5d50b8d9c82f59965b8a
Jeff Ostrin <jostrin@newrelic.com>
2019-01-18 22:53:00 -0800

    start accepting timezone parameter

--- get_schedule/get_schedule/index.js
+++ get_schedule/get_schedule/index.js
@@ -4,12 +4,31 @@ let response;
 
 exports.lambda_handler_v2 = async (event, context, callback) => {
     try {
-        const clock = new Clock();
+        const clock = new Clock("America/Los_Angeles");
         response = {
             statusCode: 200,
             body: JSON.stringify({ schedule: clock.generate_json() }),
             headers: { 'Access-Control-Allow-Origin': '*', },
         }
+    }
+    catch (err) {
+        console.log(err);
+        callback(err, null);
+    }
+
+    callback(null, response)
+};
+
+
+exports.lambda_handler_v3 = async (event, context, callback) => {
+    try {
+      var tz = require('querystring').parse(event.tz);
+      const clock = new Clock(tz);
+      response = {
+          statusCode: 200,
+          body: JSON.stringify({ schedule: clock.generate_json() }),
+          headers: { 'Access-Control-Allow-Origin': '*', },
+      }
     }
     catch (err) {
         console.log(err);
