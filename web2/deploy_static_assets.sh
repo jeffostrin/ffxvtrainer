50d87043ee0681d84cdcfe9b872717b0de399b4c
Jeff Ostrin <jostrin@newrelic.com>
2019-02-19 08:19:20 -0800

    create new s3 bucket for serving static content and add ads

--- web2/deploy_static_assets.sh
+++ web2/deploy_static_assets.sh
@@ -1,1 +1,3 @@
 aws --profile FFXVTRAINER s3 cp web/ s3://ffxv-trainer-v2 --recursive
+aws --profile FFXVTRAINER s3 cp web/ s3://ffxvtrainer.com --recursive
+aws --profile FFXVTRAINER s3 cp web/ s3://www.ffxvtrainer.com --recursive
--- web2/terraform/static_assets.tf
+++ web2/terraform/static_assets.tf
@@ -25,3 +25,58 @@ resource "aws_s3_bucket" "ffxv_s3_assets" {
 }
 POLICY
 }
+
+
+resource "aws_s3_bucket" "ffxvtrainerdotcom" {
+  bucket = "ffxvtrainer.com"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags {
+    App = "FFXV Trainer"
+  }
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": "s3:GetObject",
+          "Resource": "arn:aws:s3:::ffxvtrainer.com/*"
+      }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket" "wwwffxvtrainerdotcom" {
+  bucket = "www.ffxvtrainer.com"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags {
+    App = "FFXV Trainer"
+  }
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": "s3:GetObject",
+          "Resource": "arn:aws:s3:::www.ffxvtrainer.com/*"
+      }
+  ]
+}
+POLICY
+}
