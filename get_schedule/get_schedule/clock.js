4bae6e915cb0b9c714f7ae61211d01c11e543753
Jeff Ostrin <jostrin@newrelic.com>
2019-05-20 23:02:30 -0700

    add multi-hour-events to the json

--- get_schedule/get_schedule/clock.js
+++ get_schedule/get_schedule/clock.js
@@ -8,6 +8,7 @@ module.exports = function Clock(utcOffset) {
   var eventLoader = new EventLoader();
   var miniEventForecaster = eventLoader.load("mini_events.compact.json");
   var lunaEventForecaster = eventLoader.load("luna_gifts.compact.json");
+  var multiHourEventForecaster = eventLoader.load("multi_hour_events.compact.json");
 
   this.generate_json = function () {
     var ctime = new CTime(utcOffset);
@@ -22,8 +23,9 @@ module.exports = function Clock(utcOffset) {
       jsonHour.isCurrentHepoch = (hepoch == nowHepoch);
       jsonHour.dayTime = ctime.pp().dayTime(hepoch);
       jsonHour.relativeTime = ctime.pp().asRelativeTime(hepoch * 60 * 60 - nowSepoch);
-      jsonHour.hourly_events = miniEventForecaster.forecastFor(hepoch);;
-      jsonHour.luna_events = lunaEventForecaster.forecastFor(hepoch);;
+      jsonHour.hourly_events = miniEventForecaster.forecastFor(hepoch);
+      jsonHour.luna_events = lunaEventForecaster.forecastFor(hepoch);
+      jsonHour.multi_hour_events = multiHourEvents.forecastFor(hepoch);
       schedule.hepochs[hepoch] = jsonHour;
     }
 
